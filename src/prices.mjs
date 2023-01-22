213628035d6833cbaa44274697291eaae7d3469b
Ojala Okko <okko.ojala@hiq.fi>
2023-01-22 22:33:47 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -71,7 +71,7 @@ function createApp(database) {
 
   function calculateReduction(date) {
     let reduction = 0;
-    if (date && isMonday(date) && !isHoliday( date)) {
+    if (date && isMonday(date) && !isHoliday(date)) {
       reduction = 35;
     }
     return reduction;
